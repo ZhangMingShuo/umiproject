1774fefbf3c8c9267d3d26ca7afc375e82396a65
ZhangMingShuo <1170709161@qq.com>
2022-08-07 18:18:13 +0800

    使用plugin-layout完成项目骨架搭建:

--- config/config.js
+++ config/config.js
@@ -7,6 +7,13 @@ export default defineConfig({
   antd:{
     dark:false
   },
+  layout: {
+    // 支持任何不需要 dom 的
+    // https://procomponents.ant.design/components/layout#prolayout
+    name: 'Ant Design',
+    locale: true,
+    layout: 'side',
+  },
   routes,
   fastRefresh: {},
 });
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,12 @@
 export default [//配置式路由
-  { path: '/', component: '@/pages/index' },
-  { path: '/test', component: '@/pages/test/index' },
+  {
+    path: '/',
+    component: '@/pages/index' ,
+    name: '项目首页'
+  },
+  {
+    path: '/test',
+    component: '@/pages/test/index',
+    name: '测试'
+  },
 ]
