3b2b60151d4c076a3b1d7f36411af060ca49a8b6
ZhangMingShuo <1170709161@qq.com>
2022-08-07 19:29:48 +0800

    使用AntDesign提供的Table组件搭建学员列表

--- src/pages/stu/list.jsx
+++ src/pages/stu/list.jsx
@@ -1,7 +1,51 @@
+import { Space, Table, Tag } from 'antd';
 import React from 'react';
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: '分数',
+    dataIndex: 'score',
+    key: 'score',
+  },
+  {
+    title: '城市',
+    dataIndex: 'city',
+    key: 'city',
+  },
+  {
+    title: '生日',
+    key: 'time',
+    dataIndex: 'time',
+
+  },
+  {
+    title: '操作区域',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    score: 100,
+    city:'重庆',
+    time: '2022-10-10',
+  }
+];
 
 export default function StuList() {
   return (
-    <div>学员列表</div>
+    <Table columns={columns} dataSource={data} />
   );
 }
