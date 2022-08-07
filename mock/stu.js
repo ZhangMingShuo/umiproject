cd7c26d9530a2cb3ff2a70063b43ea8256e0c72a
ZhangMingShuo <1170709161@qq.com>
2022-08-07 19:12:18 +0800

    使用mockjs增强mock数据能力

--- mock/stu.js
+++ mock/stu.js
@@ -1,7 +1,20 @@
+import mockjs from 'mockjs'
 export default {
   // 支持值为 Object 和 Array
   'GET /classes/test': {
       username:'zhangsan',
       score:100
-   }
+   },
+  'Get /classes/stu': mockjs.mock({
+    code:200,
+    msg: '学员列表加载成功',
+    'data|100':[
+      {
+        name:'@cname',
+        score:'@integer(50,100)',
+        city:'@city',
+        time:'@date'
+      }
+    ]
+  })
 }
