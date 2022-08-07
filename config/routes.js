537264b6a160d9fc22a3caf6001cd373ff9b807c
ZhangMingShuo <1170709161@qq.com>
2022-08-07 18:48:12 +0800

    子路由配置和学生管理的下拉子菜单,包括学生列表和学生发布

--- config/routes.js
+++ config/routes.js
@@ -9,4 +9,21 @@ export default [//配置式路由
     component: '@/pages/test/index',
     name: '测试'
   },
+  {
+    path: '/stu',
+    name: '学员管理',
+    routes:[
+      {
+        path:'/stu/list',
+        component: '@/pages/stu/list',
+        name: '学员列表'
+      },
+      {
+        path:'/stu/pub',
+        component: '@/pages/stu/pub',
+        name: '学员录入'
+      }
+    ]
+  }
 ]
+
