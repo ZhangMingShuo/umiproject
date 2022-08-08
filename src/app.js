a0e1f09a324eb08bc353354fa17601894b0936ce
zhangmingshuo <1170709161@qq.com>
2022-08-08 12:26:25 +0800

    在umi中配置LeanCloud服务接口

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ export const request = {
     //请求拦截
     (url, options) => {
       console.log('请求拦截器', url, options);
-      options.url = leancloudConfig.url + url;
-      options.headers = leancloudConfig.headers;
+      options.url = leancloudConfig.url + url; //从配置文件中加载leancloud服务接口配置
+      options.headers = leancloudConfig.headers; //leancloud服务接口请求头配置
       return options; //此处是自定义请求配置
     },
   ],
