f65343d4fbab82631c4c7ba7e148b20731f14aa6
ZhangMingShuo <1170709161@qq.com>
2022-08-08 20:05:42 +0800

    useRequest手动触发执行机制

--- src/pages/category/catepub.jsx
+++ src/pages/category/catepub.jsx
@@ -1,6 +1,7 @@
-import { Button, Form, Input } from 'antd';
+import {Button, Form, Input, Spin} from 'antd';
 import { cateAdd } from '@/api/cake';
 import React from 'react';
+import {useRequest} from 'umi';
 
 const layout = {
   labelCol: {
@@ -19,13 +20,19 @@ const tailLayout = {
 
 const CatePub = () => {
   const [form] = Form.useForm();
-
+  let {data,loading,run} = useRequest(()=>{
+    console.log('useRequest执行了');
+  },{manual:true});//开启手动执行
   const onFinish = (values) => {
-    console.log('values:', values);
-    cateAdd(values).then((res) => {
-      console.log('cateAdd', res);
-    });
-    console.log(values);
+    run()
+    // console.log('values:', values);
+    // cateAdd(values).then((res) => {
+    //   console.log('cateAdd', res);
+    // });
+    // console.log(values);
+    // let {data,loading,error} = useRequest(()=>{
+    //   return cateAdd(values)
+    // })
   };
 
   const onReset = () => {
@@ -33,28 +40,30 @@ const CatePub = () => {
   };
 
   return (
-    <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-      <Form.Item
-        name="catename"
-        label="分类名称"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <Spin spinning={loading}>
+      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+        <Form.Item
+          name="catename"
+          label="分类名称"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          提交
-        </Button>
-        <Button htmlType="button" onClick={onReset}>
-          重置
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+          <Button htmlType="button" onClick={onReset}>
+            重置
+          </Button>
+        </Form.Item>
+      </Form>
+    </Spin>
   );
 };
 
